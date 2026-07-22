e13c8151d9232008df6f43f5651164508d35b7b7
Naveenmallada9090 <malladanaveen@gmail.com>
2026-07-23 00:30:05 +0530

    completed

--- dist/config.js
+++ dist/config.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.JWT_PASSWORD = void 0;
-require("dotenv/config");
+const dotenv_1 = require("dotenv");
+(0, dotenv_1.config)();
 exports.JWT_PASSWORD = process.env.JWT_PASSWORD;
